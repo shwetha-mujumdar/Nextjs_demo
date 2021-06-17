3e2da36e06c61cecfee80b463eea300b44e8f5a4
shwetha-mujumdar <smujumdar@targetclose.com>
2021-06-17 19:40:59 +0530

    changes done module4

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -4,7 +4,6 @@ import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
 import Link from "next/link";
 
-
 export default function Home() {
   const featuredEvents = getFeaturedEvents();
   console.log(featuredEvents);
@@ -15,53 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-        <div>
-          <h1>The home page details</h1>
-          <ul>
-            <li>
-              <Link href="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link href="/clients">Clients</Link>
-            </li>
-          </ul>
-        </div>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
           <div styles="display:flex">
             <EventList items={featuredEvents} />
           </div>
